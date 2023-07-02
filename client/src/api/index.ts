2372754a202a21889b5513257dc7ed8416fd33cc
Bogdan Filimon <bogdanfilimon@Bogdans-MacBook-Pro.local>
2023-07-02 15:40:52 +0300

    remove server as it is on a different repo, include credentials on requests

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -5,9 +5,10 @@ const get = async (path: string) => {
     method: "GET",
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+    credentials: "include", // include, *same-origin, omit,
     headers: {
       "Content-Type": "application/json",
+      Accept: "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
@@ -18,7 +19,7 @@ const post = async (data: any, path: string) => {
     method: "POST",
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+    credentials: "include", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
